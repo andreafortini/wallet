60cb802590b4640a5a15db9fa541982257a29909
Andrea Fortini <fortinia95@gmail.com>
2025-07-03 16:59:51 +0200

    feat: models definition

--- src/models.py
+++ src/models.py
@@ -1,1 +1,39 @@
 """Data models"""
+
+from datetime import datetime
+from enum import Enum
+
+from pydantic import BaseModel
+
+
+class Operation(Enum):
+    BUY = "BUY"
+    SELL = "SELL"
+
+
+class Category(Enum):
+    STOCK = "STOCK"
+    BOND = "BOND"
+
+
+class Orders(BaseModel):
+    ticker: str
+    isin: str
+    quantity: int
+    price: float
+    data: datetime
+    operation: Operation
+
+
+class Prices(BaseModel):
+    ticker: str
+    isin: str
+    value: float
+    date: datetime
+
+
+class Securities:
+    ticker: str
+    isin: str
+    name: str
+    category: Category
